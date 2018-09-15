3e2fb0076396335a89bf10acc1e29374808e2cf3
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2018-09-15 17:10:44 -0500

    sort: add heap

--- sort/heap.go
+++ sort/heap.go
@@ -1,5 +1,14 @@
 package sort
 
+import (
+	"github.com/rodolfo-r/datastructures"
+	"github.com/rodolfo-r/datastructures/container/priorityqueue/binaryheap"
+)
+
+// Heap applies heapsort to nums
 func Heap(nums []int) {
-		
+	var pq datastructures.PriorityQueue = binaryheap.BuildMax(nums)
+
+	for ok := true; ok; _, ok = pq.Dequeue() {
+	}
 }
